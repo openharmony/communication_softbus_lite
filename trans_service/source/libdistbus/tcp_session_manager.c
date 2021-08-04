4c639f59694bcd8e9a71c779966c4c6cbd42729d
zhufenghao <zhufenghao1@huawei.com>
2021-08-04 03:17:46 +0000

    Fixed integer overflow problem Fixed integer overflow problem

--- trans_service/source/libdistbus/tcp_session_manager.c
+++ trans_service/source/libdistbus/tcp_session_manager.c
@@ -515,6 +515,9 @@ static bool HandleRequestMsg(TcpSession *session)
     int remain = dataLen;
     while (remain > 0) {
         size = TcpRecvData(session->fd, data + total, remain, 0);
+        if (size > remain) {
+            return false;
+        }
         remain -= size;
         total += size;
     }
